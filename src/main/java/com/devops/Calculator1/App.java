f6b2efe2d3c9043ad836fd8fc723cf3d187d7c0e
chandra-4 <chandrasekharchandra888@gmail.com>
2026-03-12 01:08:42 +0530

    added bug

--- src/main/java/com/devops/Calculator1/App.java
+++ src/main/java/com/devops/Calculator1/App.java
@@ -3,6 +3,12 @@ package com.devops.Calculator1;
 public class App 
 {
 	public int add(int a, int b) {
-        return a + b;
+		
+		
+         int result = a + b;
+         
+         int x = 10/0;
+         
+         return result;
     }
 }
